088aa455fb255b35e19500b1ce595a9e78421528
Mahesh Subramanian <mahesh.subramanian@hmcts.net>
2019-09-06 21:27:56 +0100

    Bumped service-parent-pom version to 9.1.4
    Change-Id: I672a9a624ae0a80df303de6f51a7475aabaf3364

--- runIntegrationTests.sh
+++ runIntegrationTests.sh
@@ -15,7 +15,7 @@ fi
 declare -rx WILDFLY_DEPLOYMENT_DIR="${VAGRANT_DIR}/deployments"
 declare -rx CONTEXT_NAME=results
 declare -rx FRAMEWORK_VERSION=6.0.11
-declare -rx EVENT_STORE_VERSION=2.0.12
+declare -rx EVENT_STORE_VERSION=2.0.13
 
 #fail script on error
 set -e
