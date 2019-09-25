65482174949b737f52571080d41e2f2c78cabe22
Shaun Francis <shaun@mapingo.co.uk>
2019-09-25 12:20:39 +0100

    Update service parent pom to 9.1.17
    Change-Id: Ia60b1ea6c6ae6ab1e2d5ebe0f83b068d64c6ac7d

--- runIntegrationTests.sh
+++ runIntegrationTests.sh
@@ -14,8 +14,8 @@ fi
 
 declare -rx WILDFLY_DEPLOYMENT_DIR="${VAGRANT_DIR}/deployments"
 declare -rx CONTEXT_NAME=results
-declare -rx FRAMEWORK_VERSION=6.0.14
-declare -rx EVENT_STORE_VERSION=2.0.18
+declare -rx FRAMEWORK_VERSION=6.0.16
+declare -rx EVENT_STORE_VERSION=2.0.22
 
 #fail script on error
 set -e
