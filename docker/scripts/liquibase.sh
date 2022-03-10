de34f8a4e7eb54c0ab1a4c7bb3c9c7057342152b
varunvijayagopal <varun.vijayagopal@hmcts.net>
2022-03-10 09:56:27 +0000

    docker changes for results
    Change-Id: I41e69dab2c22526a03f283e25b5c9d3e1c391a9c

---
+++ docker/scripts/liquibase.sh
@@ -0,0 +1,46 @@
+echo "Running Liquibase"
+dbServerName=$1
+dbUserName=$2
+dbPassword=$3
+java -jar event-repository-liquibase.jar --url=jdbc:postgresql://${dbServerName}:5432/resultseventstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
+if [ $? -ne 0 ]
+then
+    exit 1
+else
+    echo success!
+fi
+java -jar aggregate-snapshot-repository-liquibase.jar --url=jdbc:postgresql://${dbServerName}:5432/resultseventstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
+if [ $? -ne 0 ]
+then
+    exit 1
+else
+    echo success!
+fi
+java -jar event-buffer-liquibase.jar --url=jdbc:postgresql://${dbServerName}:5432/resultsviewstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
+if [ $? -ne 0 ]
+then
+    exit 1
+else
+    echo success!
+fi
+java -jar event-tracking-liquibase.jar --url=jdbc:postgresql://${dbServerName}:5432/resultsviewstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
+if [ $? -ne 0 ]
+then
+    exit 1
+else
+    echo success!
+fi
+java -jar results-viewstore-liquibase.jar --url=jdbc:postgresql://${dbServerName}:5432/resultsviewstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
+if [ $? -ne 0 ]
+then
+    exit 1
+else
+    echo success!
+fi
+java -jar framework-system-liquibase.jar --url=jdbc:postgresql://${dbServerName}:5432/resultssystem?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
+if [ $? -ne 0 ]
+then
+    exit 1
+else
+    echo success!
+fi
