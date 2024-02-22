3e3ab263efba20c08db6b9a5304e41059e246ae9
gunnertwin <daniel@alvestechnology.io>
2024-02-22 15:59:36 +0000

    Update liquibase script to use dynamic dbPort
    Change-Id: Iee1bc4928bf94238ff44aa60749a7ec7d3b639b3

--- docker/scripts/liquibase.sh
+++ docker/scripts/liquibase.sh
@@ -2,42 +2,43 @@ echo "Running Liquibase"
 dbServerName=$1
 dbUserName=$2
 dbPassword=$3
-java -jar event-repository-liquibase.jar --url=jdbc:postgresql://${dbServerName}:5432/resultseventstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
+dbPort=${5:-5432}
+java -jar event-repository-liquibase.jar --url=jdbc:postgresql://${dbServerName}:${dbPort}/resultseventstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
 if [ $? -ne 0 ]
 then
     exit 1
 else
     echo success!
 fi
-java -jar aggregate-snapshot-repository-liquibase.jar --url=jdbc:postgresql://${dbServerName}:5432/resultseventstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
+java -jar aggregate-snapshot-repository-liquibase.jar --url=jdbc:postgresql://${dbServerName}:${dbPort}/resultseventstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
 if [ $? -ne 0 ]
 then
     exit 1
 else
     echo success!
 fi
-java -jar event-buffer-liquibase.jar --url=jdbc:postgresql://${dbServerName}:5432/resultsviewstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
+java -jar event-buffer-liquibase.jar --url=jdbc:postgresql://${dbServerName}:${dbPort}/resultsviewstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
 if [ $? -ne 0 ]
 then
     exit 1
 else
     echo success!
 fi
-java -jar event-tracking-liquibase.jar --url=jdbc:postgresql://${dbServerName}:5432/resultsviewstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
+java -jar event-tracking-liquibase.jar --url=jdbc:postgresql://${dbServerName}:${dbPort}/resultsviewstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
 if [ $? -ne 0 ]
 then
     exit 1
 else
     echo success!
 fi
-java -jar results-viewstore-liquibase.jar --url=jdbc:postgresql://${dbServerName}:5432/resultsviewstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
+java -jar results-viewstore-liquibase.jar --url=jdbc:postgresql://${dbServerName}:${dbPort}/resultsviewstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
 if [ $? -ne 0 ]
 then
     exit 1
 else
     echo success!
 fi
-java -jar framework-system-liquibase.jar --url=jdbc:postgresql://${dbServerName}:5432/resultssystem?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
+java -jar framework-system-liquibase.jar --url=jdbc:postgresql://${dbServerName}:${dbPort}/resultssystem?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
 if [ $? -ne 0 ]
 then
     exit 1
