79463b141625f545cbee93b55f4b14afb82a6513
srivani.muddineni  <srivani.muddineni@hmcts.net>
2025-09-11 21:28:32 +0100

    changes

--- results-event/results-event-listener/src/main/java/uk/gov/moj/cpp/results/event/HearingFinancialResultsUpdatedListener.java
+++ results-event/results-event-listener/src/main/java/uk/gov/moj/cpp/results/event/HearingFinancialResultsUpdatedListener.java
@@ -1,6 +1,5 @@
 package uk.gov.moj.cpp.results.event;
 
-import static java.util.UUID.randomUUID;
 import static uk.gov.justice.services.core.annotation.Component.EVENT_LISTENER;
 
 import uk.gov.justice.core.courts.HearingFinancialResultsUpdated;
@@ -15,11 +14,7 @@ import java.time.ZonedDateTime;
 import java.util.UUID;
 
 import javax.inject.Inject;
-import javax.json.JsonArray;
-import javax.json.JsonArrayBuilder;
 import javax.json.JsonObject;
-
-import static javax.json.Json.createArrayBuilder;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -38,7 +33,6 @@ public class HearingFinancialResultsUpdatedListener {
     public void handleDefendantGobAccounts(final JsonEnvelope event) {
         final JsonObject requestJson = event.payloadAsJsonObject();
         final HearingFinancialResultsUpdated hearingFinancialResultsUpdated = jsonObjectToObjectConverter.convert(requestJson, HearingFinancialResultsUpdated.class);
-        //updateDefendantGobAccounts(hearingFinancialResultsUpdated);
         final UUID masterDefendantId = hearingFinancialResultsUpdated.getMasterDefendantId();
         final UUID correlationId = hearingFinancialResultsUpdated.getCorrelationId();
         final UUID hearingId = hearingFinancialResultsUpdated.getHearingId();
@@ -55,45 +49,4 @@ public class HearingFinancialResultsUpdatedListener {
             LOGGER.info("GOB account Details updated with account_number for masterDefendantId id & correlationId id & hearingId : {} & {} & {}", masterDefendantId, correlationId, hearingId);
         }
     }
-
-    private void updateDefendantGobAccounts(HearingFinancialResultsUpdated hearingFinancialResultsUpdated) {
-        final UUID masterDefendantId = hearingFinancialResultsUpdated.getMasterDefendantId();
-        final UUID correlationId = hearingFinancialResultsUpdated.getCorrelationId();
-        final UUID hearingId = hearingFinancialResultsUpdated.getHearingId();
-        
-        // Find existing record to update with account_number
-        final DefendantGobAccountsEntity existingEntity = defendantGobAccountsRepository.findAccountNumberByMasterDefendantIdAndHearingIdAndCorrelationId(masterDefendantId, hearingId, correlationId);
-        
-        if (existingEntity != null) {
-            // Update existing record with account_number and account_request_time
-            existingEntity.setAccountNumber(hearingFinancialResultsUpdated.getAccountNumber());
-            existingEntity.setAccountRequestTime(hearingFinancialResultsUpdated.getAccountRequestTime());
-            existingEntity.setUpdateTime(ZonedDateTime.now());
-            defendantGobAccountsRepository.save(existingEntity);
-            LOGGER.info("GOB account Details updated with account_number for masterDefendantId id & correlationId id & hearingId : {} & {} & {}", masterDefendantId, correlationId, hearingId);
-        } /*else {
-            // Fallback: create new record if none exists (shouldn't happen in normal flow)
-            defendantGobAccountsRepository.save(updateDefendantGobAccountsEntity(hearingFinancialResultsUpdated));
-            LOGGER.warn("No existing GOB account record found, created new one for masterDefendantId id & correlationId id & hearingId : {} & {} & {}", masterDefendantId, correlationId, hearingId);
-        }*/
-    }
-
-    /*private DefendantGobAccountsEntity updateDefendantGobAccountsEntity(HearingFinancialResultsUpdated hearingFinancialResultsUpdated) {
-        final DefendantGobAccountsEntity defendantGobAccountsEntity = new DefendantGobAccountsEntity();
-        defendantGobAccountsEntity.setId(randomUUID());
-        defendantGobAccountsEntity.setMasterDefendantId(hearingFinancialResultsUpdated.getMasterDefendantId());
-        defendantGobAccountsEntity.setCorrelationId(hearingFinancialResultsUpdated.getCorrelationId());
-        defendantGobAccountsEntity.setAccountNumber(hearingFinancialResultsUpdated.getAccountNumber());
-        defendantGobAccountsEntity.setAccountRequestTime(hearingFinancialResultsUpdated.getAccountRequestTime());
-        defendantGobAccountsEntity.setHearingId(hearingFinancialResultsUpdated.getHearingId());
-        return defendantGobAccountsEntity;
-    }
-
-    private JsonArray convertCaseReferencesToJsonArray(final java.util.List<String> caseReferences) {
-        final JsonArrayBuilder arrayBuilder = createArrayBuilder();
-        if (caseReferences != null) {
-            caseReferences.forEach(arrayBuilder::add);
-        }
-        return arrayBuilder.build();
-    }*/
 }
