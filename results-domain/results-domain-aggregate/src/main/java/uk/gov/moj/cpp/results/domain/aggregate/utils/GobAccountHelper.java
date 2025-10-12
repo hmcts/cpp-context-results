1920ff24022b807a02185a31fea815a9ca4536fa
Natraj Molala <natraj.molala3@hmcts.net>
2025-10-12 16:48:48 +0100

    [DD-40524] track hearingId in the correlationId; use hearingId to determine oldGobAccounts

--- results-domain/results-domain-aggregate/src/main/java/uk/gov/moj/cpp/results/domain/aggregate/utils/GobAccountHelper.java
+++ results-domain/results-domain-aggregate/src/main/java/uk/gov/moj/cpp/results/domain/aggregate/utils/GobAccountHelper.java
@@ -1,7 +1,10 @@
 package uk.gov.moj.cpp.results.domain.aggregate.utils;
 
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
 import static java.util.Comparator.comparing;
-import static java.util.stream.Collectors.toList;
+import static java.util.Objects.nonNull;
+import static org.apache.commons.collections.CollectionUtils.isNotEmpty;
 import static uk.gov.moj.cpp.results.domain.aggregate.NCESDecisionHelper.isApplicationDenied;
 
 import uk.gov.justice.hearing.courts.OffenceResultsDetails;
@@ -11,22 +14,38 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 public class GobAccountHelper {
 
     public static List<String> getOldGobAccounts(final LinkedList<CorrelationItem> correlationItemList, final UUID accountCorrelationId, final List<UUID> offenceIdList,
                                                  final Map<UUID, List<OffenceResultsDetails>> applicationResultsDetails) {
-        return offenceIdList.stream()
-                .map(offenceId -> getOldGobAccount(correlationItemList, accountCorrelationId, offenceId, applicationResultsDetails))
+
+        correlationItemList.sort(comparing(CorrelationItem::getCreatedTime).reversed());
+        final List<CorrelationItem> oldCorrelationItemsMatchingOffences = offenceIdList.stream()
+                .map(offenceId -> getOldCorrelationItemMatch(correlationItemList, accountCorrelationId, offenceId, applicationResultsDetails))
                 .filter(Objects::nonNull)
                 .distinct()
-                .collect(toList());
+                .toList();
+
+        final boolean isSingleHearing = oldCorrelationItemsMatchingOffences.stream().map(CorrelationItem::getHearingId).filter(Objects::nonNull).distinct().count() == 1;
+        if (isSingleHearing) {
+            return isNotEmpty(oldCorrelationItemsMatchingOffences)
+                    ? singletonList(oldCorrelationItemsMatchingOffences.get(oldCorrelationItemsMatchingOffences.size() - 1).getAccountNumber())
+                    : emptyList();
+        } else {
+            return oldCorrelationItemsMatchingOffences.stream().map(CorrelationItem::getAccountNumber).filter(Objects::nonNull).distinct().toList();
+        }
     }
 
     public static String getOldGobAccount(final LinkedList<CorrelationItem> correlationItemList, final UUID accountCorrelationId, final UUID offenceId,
                                           final Map<UUID, List<OffenceResultsDetails>> applicationResultsDetails) {
         correlationItemList.sort(comparing(CorrelationItem::getCreatedTime).reversed());
+        final CorrelationItem oldCorrelationItemMatch = getOldCorrelationItemMatch(correlationItemList, accountCorrelationId, offenceId, applicationResultsDetails);
+        return nonNull(oldCorrelationItemMatch) ? oldCorrelationItemMatch.getAccountNumber() : null;
+    }
+
+    private static CorrelationItem getOldCorrelationItemMatch(final LinkedList<CorrelationItem> correlationItemList, final UUID accountCorrelationId, final UUID offenceId,
+                                                              final Map<UUID, List<OffenceResultsDetails>> applicationResultsDetails) {
 
         //find previous matching correlationItem by offenceId; then ensure the offence isFinancial to return valid GobAccountNumber
         return correlationItemList.stream()
@@ -45,7 +64,7 @@ public class GobAccountHelper {
                             .filter(o -> o.getOffenceId().equals(offenceId))
                             .findFirst()
                             .filter(offenceResultsMatch -> Boolean.TRUE.equals(offenceResultsMatch.getIsFinancial())).isPresent()) {
-                        return correlationItem.getAccountNumber();
+                        return correlationItem;
                     }
                     return null;
                 }).orElse(null);
--- results-domain/results-domain-aggregate/src/test/java/uk/gov/moj/cpp/results/domain/aggregate/utils/GobAccountHelperTest.java
+++ results-domain/results-domain-aggregate/src/test/java/uk/gov/moj/cpp/results/domain/aggregate/utils/GobAccountHelperTest.java
@@ -5,20 +5,19 @@ import static java.util.List.of;
 import static java.util.UUID.randomUUID;
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.anEmptyMap;
 import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 import static uk.gov.justice.hearing.courts.OffenceResultsDetails.offenceResultsDetails;
+import static uk.gov.moj.cpp.results.domain.aggregate.utils.GobAccountHelper.getOldGobAccounts;
 
 import uk.gov.justice.hearing.courts.OffenceResultsDetails;
 import uk.gov.moj.cpp.results.domain.event.ImpositionOffenceDetails;
 
 import java.time.ZonedDateTime;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -38,13 +37,14 @@ public class GobAccountHelperTest {
     public void givenOffenceResultedFPInACaseAndGobExists_offenceResultedFPInAnApplicationShouldGetPreviousGobFromCase() {
 
         final UUID offenceId = randomUUID();
+        final UUID hearingId = randomUUID();
 
         final OffenceResultsDetails caseOffence = getOffenceResultsDetails(offenceId, true);
-        final CorrelationItem correlationItemCase = getCorrelation(of(caseOffence), "GOB1");
+        final CorrelationItem correlationItemCase = getCorrelation(hearingId, of(caseOffence), "GOB1");
         correlationItemList.add(correlationItemCase);
 
         final OffenceResultsDetails applicationOffence = getOffenceResultsDetails(offenceId, true);
-        final CorrelationItem correlationItemApp = getCorrelation(of(applicationOffence), "GOB2");
+        final CorrelationItem correlationItemApp = getCorrelation(hearingId, of(applicationOffence), "GOB2");
         correlationItemList.add(correlationItemApp);
 
         final String oldGobAccount = GobAccountHelper.getOldGobAccount(correlationItemList, correlationItemApp.getAccountCorrelationId(), applicationOffence.getOffenceId(), EMPTY_MAP);
@@ -55,16 +55,17 @@ public class GobAccountHelperTest {
     public void givenOffenceResultedFPInACaseAndGobExists_offenceResultedFPInAnApp1_offenceResultedFPInAnApp2_ShouldGetPreviousGobFromApp1() {
 
         final UUID offenceId = randomUUID();
+        final UUID hearingId = randomUUID();
 
         final OffenceResultsDetails caseOffence = getOffenceResultsDetails(offenceId, true);
-        correlationItemList.add(getCorrelation(List.of(caseOffence), "GOB1"));
+        correlationItemList.add(getCorrelation(hearingId, List.of(caseOffence), "GOB1"));
 
         final OffenceResultsDetails application1Offence = getOffenceResultsDetails(offenceId, true);
-        final CorrelationItem correlationItemApp = getCorrelation(of(application1Offence), "GOB2");
+        final CorrelationItem correlationItemApp = getCorrelation(hearingId, of(application1Offence), "GOB2");
         correlationItemList.add(correlationItemApp);
 
         final OffenceResultsDetails application2Offence = getOffenceResultsDetails(offenceId, true);
-        final CorrelationItem correlationItemApp2 = getCorrelation(of(application2Offence), "GOB3");
+        final CorrelationItem correlationItemApp2 = getCorrelation(hearingId, of(application2Offence), "GOB3");
         correlationItemList.add(correlationItemApp2);
 
         final String oldGobAccount = GobAccountHelper.getOldGobAccount(correlationItemList, correlationItemApp2.getAccountCorrelationId(), application2Offence.getOffenceId(), EMPTY_MAP);
@@ -75,16 +76,17 @@ public class GobAccountHelperTest {
     public void givenOffenceResultedFPInACaseAndGobExists_offenceResultedNonFPInAnApp1_offenceResultedFPInAnApp2_ShouldNotGetPreviousGobFromApp1() {
 
         final UUID offenceId = randomUUID();
+        final UUID hearingId = randomUUID();
 
         final OffenceResultsDetails caseOffence = getOffenceResultsDetails(offenceId, true);
-        correlationItemList.add(getCorrelation(List.of(caseOffence), "GOB1"));
+        correlationItemList.add(getCorrelation(hearingId, List.of(caseOffence), "GOB1"));
 
         final OffenceResultsDetails application1Offence = getOffenceResultsDetails(offenceId, false);
-        final CorrelationItem correlationItemApp = getCorrelation(of(application1Offence), "GOB2");
+        final CorrelationItem correlationItemApp = getCorrelation(hearingId, of(application1Offence), "GOB2");
         correlationItemList.add(correlationItemApp);
 
         final OffenceResultsDetails application2Offence = getOffenceResultsDetails(offenceId, true);
-        final CorrelationItem correlationItemApp2 = getCorrelation(of(application2Offence), "GOB3");
+        final CorrelationItem correlationItemApp2 = getCorrelation(hearingId, of(application2Offence), "GOB3");
         correlationItemList.add(correlationItemApp2);
 
         final String oldGobAccount = GobAccountHelper.getOldGobAccount(correlationItemList, correlationItemApp2.getAccountCorrelationId(), application2Offence.getOffenceId(), EMPTY_MAP);
@@ -92,23 +94,25 @@ public class GobAccountHelperTest {
     }
 
     @Test
-    public void givenMultipleOffencesResultedFPInACaseAndGobExists_offenceResultedFPInAnApp1_offenceResultedFPInAnApp2_ShouldGetPreviousGobFromApp1() {
+    public void givenMultipleOffencesResultedFPInASingleHearingAndGobCreated_shouldGetSingleAndMostRecentGobAccountFromPreviousGobs() {
 
         final UUID offenceId1 = randomUUID();
         final UUID offenceId2 = randomUUID();
         final UUID offenceId3 = randomUUID();
+        final UUID hearingId = randomUUID();
+
 
         //case level o1, o2, o3 - GOB1
         final List<OffenceResultsDetails> caseOffences = getOffenceResultsDetails(List.of(offenceId1, offenceId2, offenceId3));
-        correlationItemList.add(getCorrelation(caseOffences, "GOB1"));
+        correlationItemList.add(getCorrelation(hearingId, caseOffences, "GOB1"));
 
         //App1 level o1, o3 - GOB2
         final List<OffenceResultsDetails> application1Offences = getOffenceResultsDetails(List.of(offenceId1, offenceId3));
-        correlationItemList.add(getCorrelation(application1Offences, "GOB2"));
+        correlationItemList.add(getCorrelation(hearingId, application1Offences, "GOB2"));
 
         //App2 level o2 - GOB3
         final OffenceResultsDetails application2Offence = getOffenceResultsDetails(offenceId2, true);
-        final CorrelationItem correlationItemApp2 = getCorrelation(of(application2Offence), "GOB3");
+        final CorrelationItem correlationItemApp2 = getCorrelation(hearingId, of(application2Offence), "GOB3");
         correlationItemList.add(correlationItemApp2);
 
         final String oldGobAccount = GobAccountHelper.getOldGobAccount(correlationItemList, correlationItemApp2.getAccountCorrelationId(), application2Offence.getOffenceId(), EMPTY_MAP);
@@ -116,7 +120,7 @@ public class GobAccountHelperTest {
 
         //App3 level o3 - GOB4
         final OffenceResultsDetails application3Offence = getOffenceResultsDetails(offenceId3, true);
-        final CorrelationItem correlationItemApp3 = getCorrelation(of(application3Offence), "GOB4");
+        final CorrelationItem correlationItemApp3 = getCorrelation(hearingId, of(application3Offence), "GOB4");
         correlationItemList.add(correlationItemApp3);
 
         final String oldGobAccountForApp3 = GobAccountHelper.getOldGobAccount(correlationItemList, correlationItemApp3.getAccountCorrelationId(), application3Offence.getOffenceId(), EMPTY_MAP);
@@ -124,12 +128,41 @@ public class GobAccountHelperTest {
 
         //App4 level o2, o3 - GOB4
         final List<OffenceResultsDetails> application4Offences = getOffenceResultsDetails(List.of(offenceId2, offenceId3));
-        final CorrelationItem correlationItemApp4 = getCorrelation(application4Offences, "GOB5");
+        final CorrelationItem correlationItemApp4 = getCorrelation(hearingId, application4Offences, "GOB5");
         correlationItemList.add(correlationItemApp4);
 
-        final List<String> oldGobAccountsForApp4 = GobAccountHelper.getOldGobAccounts(correlationItemList, correlationItemApp4.getAccountCorrelationId(), List.of(offenceId2, offenceId3), EMPTY_MAP);
+        final List<String> oldGobAccountsForApp4 = getOldGobAccounts(correlationItemList, correlationItemApp4.getAccountCorrelationId(), List.of(offenceId2, offenceId3), EMPTY_MAP);
+        assertThat(oldGobAccountsForApp4.size(), is(1));
+        assertThat(oldGobAccountsForApp4, contains("GOB4"));
+    }
+
+    @Test
+    public void givenMultipleOffencesResultedFPInDifferentHearingsAndGobCreated_shouldGetSingleAndMostRecentGobAccountFromPreviousGobs() {
+
+        final UUID offenceId1 = randomUUID();
+        final UUID offenceId2 = randomUUID();
+        final UUID offenceId3 = randomUUID();
+        final UUID hearingId1 = randomUUID();
+        final UUID hearingId2 = randomUUID();
+        final UUID hearingId3 = randomUUID();
+
+        //case level o1, o2, o3 - GOB1
+        final List<OffenceResultsDetails> caseOffences = getOffenceResultsDetails(List.of(offenceId1, offenceId2, offenceId3));
+        correlationItemList.add(getCorrelation(hearingId1, caseOffences, "GOB1"));
+
+        //case amend o1, o3 - GOB2
+        final List<OffenceResultsDetails> caseAmendOffences = getOffenceResultsDetails(List.of(offenceId1, offenceId3));
+        correlationItemList.add(getCorrelation(hearingId2, caseAmendOffences, "GOB2"));
+
+
+        //App2 level o3 - GOB4
+        final CorrelationItem correlationItemApp2 = getCorrelation(hearingId3, of(getOffenceResultsDetails(offenceId1, true), getOffenceResultsDetails(offenceId2, false),
+                getOffenceResultsDetails(offenceId3, true)), "GOB3");
+        correlationItemList.add(correlationItemApp2);
+
+        final List<String> oldGobAccountsForApp4 = getOldGobAccounts(correlationItemList, correlationItemApp2.getAccountCorrelationId(), List.of(offenceId1, offenceId2, offenceId3), EMPTY_MAP);
         assertThat(oldGobAccountsForApp4.size(), is(2));
-        assertThat(oldGobAccountsForApp4, contains("GOB3", "GOB4"));
+        assertThat(oldGobAccountsForApp4, containsInAnyOrder("GOB1", "GOB2"));
     }
 
     private static OffenceResultsDetails getOffenceResultsDetails(final UUID offenceId1, final boolean isFinancial) {
@@ -151,9 +184,10 @@ public class GobAccountHelperTest {
                 .collect(Collectors.toList());
     }
 
-    private static CorrelationItem getCorrelation(final List<OffenceResultsDetails> caseOffences, final String gobAccount) {
+    private static CorrelationItem getCorrelation(final UUID hearingId, final List<OffenceResultsDetails> caseOffences, final String gobAccount) {
         return CorrelationItem.correlationItem()
                 .withAccountCorrelationId(randomUUID())
+                .withHearingId(hearingId)
                 .withAccountDivisionCode(randomAlphanumeric(8))
                 .withCreatedTime(ZonedDateTime.now())
                 .withProsecutionCaseReferences(of("CASEURN001"))
