628eab5296f02723af79116bc2f0d4dba267d666
AnumulaBhagyaLakshmi <bhagi.2020@gmail.com>
2025-11-12 13:26:59 +0000

    SNI-744

--- results-event/results-event-processor/src/main/java/uk/gov/moj/cpp/results/event/helper/results/CommonMethods.java
+++ results-event/results-event-processor/src/main/java/uk/gov/moj/cpp/results/event/helper/results/CommonMethods.java
@@ -17,6 +17,8 @@ import uk.gov.justice.core.courts.HearingDay;
 import uk.gov.justice.core.courts.MasterDefendant;
 import uk.gov.justice.core.courts.ProsecutionCaseIdentifier;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -44,7 +46,8 @@ public class CommonMethods {
         String result = N;
         final Optional<ZonedDateTime> sittingDayOptional = hearing.getHearingDays().stream().map(HearingDay::getSittingDay).findFirst();
         if (null != attendanceDays && sittingDayOptional.isPresent()) {
-            final Optional<AttendanceDay> attendanceDay = attendanceDays.stream().filter(a -> a.getDay().equals(sittingDayOptional.get().toLocalDate()) && a.getAttendanceType() != AttendanceType.NOT_PRESENT).findFirst();
+            final Optional<AttendanceDay> attendanceDay = attendanceDays.stream()
+                    .filter(a -> a.getDay().equals(getLocalLondonZoneDate(sittingDayOptional.get())) && a.getAttendanceType() != AttendanceType.NOT_PRESENT).findFirst();
             if (attendanceDay.isPresent()) {
                 result = Y;
             }
@@ -59,7 +62,7 @@ public class CommonMethods {
         String result = N;
         final Optional<ZonedDateTime> sittingDayOptional = hearing.getHearingDays().stream().map(HearingDay::getSittingDay).findFirst();
         if (null != attendanceDays && sittingDayOptional.isPresent()) {
-            final Optional<AttendanceDay> attendanceDay = attendanceDays.stream().filter(a -> a.getDay().equals(sittingDayOptional.get().toLocalDate()) && a.getAttendanceType() != AttendanceType.NOT_PRESENT).findFirst();
+            final Optional<AttendanceDay> attendanceDay = attendanceDays.stream().filter(a -> a.getDay().equals(getLocalLondonZoneDate(sittingDayOptional.get())) && a.getAttendanceType() != AttendanceType.NOT_PRESENT).findFirst();
             if (attendanceDay.isPresent()) {
                 result = Y;
             }
@@ -74,7 +77,7 @@ public class CommonMethods {
         Optional<UUID> defendantDefenceCounsel = empty();
         if (null != hearing.getDefenceCounsels() && sittingDayOptional.isPresent()) {
             defendantDefenceCounsel = hearing.getDefenceCounsels().stream()
-                    .filter(d -> d.getAttendanceDays().contains(sittingDayOptional.get().toLocalDate()))
+                    .filter(d -> d.getAttendanceDays().contains(getLocalLondonZoneDate(sittingDayOptional.get())))
                     .map(DefenceCounsel::getDefendants)
                     .flatMap(Collection::stream)
                     .filter(a -> a.equals(defendant.getId())).findFirst();
@@ -82,11 +85,15 @@ public class CommonMethods {
         return defendantDefenceCounsel;
     }
 
+    private static LocalDate getLocalLondonZoneDate(ZonedDateTime utcDateTime){
+        return utcDateTime.withZoneSameInstant(ZoneId.of("Europe/London")).toLocalDate();
+    }
+
     private static Optional<UUID> getDefenceCounsel(final Hearing hearing, final MasterDefendant defendant, final Optional<ZonedDateTime> sittingDayOptional) {
         Optional<UUID> defendantDefenceCounsel = empty();
         if (null != hearing.getDefenceCounsels() && sittingDayOptional.isPresent()) {
             defendantDefenceCounsel = hearing.getDefenceCounsels().stream()
-                    .filter(d -> d.getAttendanceDays().contains(sittingDayOptional.get().toLocalDate()))
+                    .filter(d -> d.getAttendanceDays().contains(getLocalLondonZoneDate(sittingDayOptional.get())))
                     .map(DefenceCounsel::getDefendants)
                     .flatMap(Collection::stream)
                     .filter(a -> a.equals(defendant.getMasterDefendantId())).findFirst();
