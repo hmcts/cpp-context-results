fe2db4cd16e6ed5ec2138fe83afa5feb51597fa8
aykutdanisman <aykutdanisman@gmail.com>
2026-01-27 15:57:41 +0000

    CHD-1784 - Framework E upgrade (#92)
    * CHD-1784 - Framework E upgrade
    
    * fix review comments
    
    * fix review comments

--- results-event/results-event-processor/src/main/java/uk/gov/moj/cpp/results/event/service/SystemDocGenerator.java
+++ results-event/results-event-processor/src/main/java/uk/gov/moj/cpp/results/event/service/SystemDocGenerator.java
@@ -1,6 +1,7 @@
 package uk.gov.moj.cpp.results.event.service;
 
 import static uk.gov.justice.services.messaging.JsonObjects.createObjectBuilder;
+import static uk.gov.justice.services.messaging.JsonObjects.createArrayBuilder;
 import static org.apache.commons.collections.MapUtils.isNotEmpty;
 import static uk.gov.justice.services.core.annotation.Component.EVENT_PROCESSOR;
 import static uk.gov.justice.services.messaging.Envelope.metadataFrom;
@@ -13,7 +14,6 @@ import uk.gov.justice.services.messaging.JsonEnvelope;
 import java.util.Map;
 
 import javax.inject.Inject;
-import uk.gov.justice.services.messaging.JsonObjects;
 import javax.json.JsonArrayBuilder;
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
@@ -42,7 +42,7 @@ public class SystemDocGenerator {
                 .add("payloadFileServiceId", request.getPayloadFileServiceId().toString());
 
         if (isNotEmpty(request.getAdditionalInformation())) {
-            JsonArrayBuilder infoArrayBuilder = JsonObjects.createArrayBuilder();
+            JsonArrayBuilder infoArrayBuilder = createArrayBuilder();
             final Map<String, String> additionalInfo = request.getAdditionalInformation();
             additionalInfo.forEach((k, v) ->
                     infoArrayBuilder.add(createObjectBuilder()
